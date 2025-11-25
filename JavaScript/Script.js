6a98b8a29f15a62e1e44cb76e32ce2d7253d0a0c
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-11-25 23:58:46 +0530

    Update Script.js

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,13 +1,16 @@
 document.getElementById("loginForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // Prevent page refresh
+  e.preventDefault(); 
+  
+  // Prevent page refresh
 
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   const message = document.getElementById("message");
 
   // Example credentials (replace with database or API check)
+  
   const validUsername = "admin";
-  const validPassword = "12345";
+  const validPassword = "12348";
 
   if (username === validUsername && password === validPassword) {
     message.style.color = "green";
